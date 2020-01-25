471430b188d9d181846f0f3e72883592172d2cb6
soun059 <soujanna2015newera@gmail.com>
2020-01-25 16:03:52 +0530

    Updates

---
+++ precache-manifest.66301df30e57b6d35af34f6d9c3a3e16.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba93b49c3436548f20c8f3c7b855fea",
+    "url": "/portfl/index.html"
+  },
+  {
+    "revision": "6f056e0af20bbe8aa067",
+    "url": "/portfl/static/css/main.6ef8fc7d.chunk.css"
+  },
+  {
+    "revision": "98765957094d654ffe95",
+    "url": "/portfl/static/js/2.7a2069d7.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/portfl/static/js/2.7a2069d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f056e0af20bbe8aa067",
+    "url": "/portfl/static/js/main.89f5c9f5.chunk.js"
+  },
+  {
+    "revision": "6f5f32d547faf96175f7",
+    "url": "/portfl/static/js/runtime-main.62fed4e3.js"
+  },
+  {
+    "revision": "ba6433b713805b414dbd73b03b77fbaf",
+    "url": "/portfl/static/media/coding.ba6433b7.jpg"
+  },
+  {
+    "revision": "626e7f1a56a515ee7f466b947b60fb4e",
+    "url": "/portfl/static/media/me.626e7f1a.jpg"
+  },
+  {
+    "revision": "341d5da47e7ff5f94fe7ca38f8082ce2",
+    "url": "/portfl/static/media/wall.341d5da4.png"
+  },
+  {
+    "revision": "b230b7e6676a073b02d7f49438923d47",
+    "url": "/portfl/static/media/wall.b230b7e6.svg"
+  }
+]);
